30baea5254539a5d144195e40fbe24b14811ba2b
Blake Fletcher <blkfltchr@gmail.com>
2019-02-10 15:38:25 -0500

    Authenticated logged in user with JWT

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys')
+const passport = require('passport');
 
 // Load user model
 const User = require('../../models/User')
@@ -65,12 +68,38 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if(isMatch) {
-            res.json({msg: 'Success'})
+            // User matched
+
+            // Create JWT payload
+            const payload = { id: user.id, name: user.name, avatar: user.avatar }
+
+            // Sign token
+            jwt.sign(
+              payload, 
+              keys.secretOrKey, 
+              { expiresIn: 3600 }, 
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                })
+            });
           } else {
             return res.status(400).json({password: 'You have entered an invalid email or password.'});
           }
         })
     })
+})
+
+// ROUTE:   GET api/users/current
+// DESC:    Return Current User
+// ACCESS:  Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
 })
 
 module.exports = router;
