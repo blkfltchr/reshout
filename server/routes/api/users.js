4c66060fac34360b2bf5604e399892cf635d3415
Blake Fletcher <blkfltchr@gmail.com>
2019-02-11 12:14:21 -0500

    Register and login validation completed

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys')
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 // Load user model
 const User = require('../../models/User')
 
@@ -14,45 +18,60 @@ const User = require('../../models/User')
 // ACCESS:  Public
 router.get('/test', (req, res) => res.json({msg: 'Users works'}));
 
-// ROUTE:   POST api/users/register
-// DESC:    Register User
-// ACCESS:  Public
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
 router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if(user) {
-        return res.status(400).json({email: 'Email already exists'})
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: '200', // Size
-          r: 'pg', // Rating
-          d: 'mm' // Default
-        });
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-          avatar
-        });
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          })
-        })
-      }
-    })
-})
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 // ROUTE:   GET api/users/login
 // DESC:    Login User / Return JWT Token
 // ACCESS:  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -61,7 +80,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if(!user) {
-        return res.status(404).json({email: 'You have entered an invalid email or password.'});
+        errors.email = 'You have entered an invalid email or password.';
+        return res.status(404).json(errors);
       }
 
       // Check password
@@ -85,7 +105,8 @@ router.post('/login', (req, res) => {
                 })
             });
           } else {
-            return res.status(400).json({password: 'You have entered an invalid email or password.'});
+            errors.password = 'You have entered an invalid email or password.'
+            return res.status(400).json(errors);
           }
         })
     })
